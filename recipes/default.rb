822ca1fc036d306b51cde113b38bf7dc26d45d6d
Umang Chouhan <uchouhan@optimiscorp.com>
2014-02-22 12:32:47 +0530

    Adding a recipe to update the variables file.

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,23 @@
 # Cookbook Name:: chef-env-vars
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Copyright 2014, OptimisCorp.
 #
 # All rights reserved - Do Not Redistribute
 #
+
+file_content = node['vars'].collect do |variable|
+  databag = variable['databag']
+  item = variable['item']
+  name = variable['name']
+  encrypted = variable['encrypted'] == false ? false : true
+  provider = encrypted ? Chef::EncryptedDataBagItem : Chef::DataBagItem
+  bag = provider.load databag, item
+  value = bag[name]
+
+  "export #{name}=#{value}"
+end
+
+file '/etc/profile.d/variables.sh' do
+  content file_content.join('\n')
+end
