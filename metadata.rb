52ee7e7c812b83f03a6abeda0514803fd138e86e
Umang Chouhan <uchouhan@optimiscorp.com>
2014-02-22 08:20:04 +0530

    Generating stock cookbook using Knife.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-env-vars'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef-env-vars'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: chef-env-vars
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
